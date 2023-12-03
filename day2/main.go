f0699866e4e4007c28e3e20453e49329cfa0378c
dukendev <ysanjeet535@gmail.com>
2023-12-03 13:58:32 +0530

    started with day2 problem, setup complete

--- day2/main.go
+++ day2/main.go
@@ -1,9 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-  fmt.Println("Hello Go") 
-  
-  
+	filePath := "/Users/sanjeetyadav/dev/adventOfCode2023/advent-of-code-2023-go/day2/input.txt"
+	lines, err := ReadLines(filePath)
+	check(err)
+	fmt.Println(lines)
+}
+
+func ReadLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, err
+	}
+
+	return lines, nil
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
