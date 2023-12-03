65373d8cf49c9c91f70959de516702da68ce9995
dukendev <ysanjeet535@gmail.com>
2023-12-04 02:28:38 +0530

    solved day2 part 1

--- day2/main.go
+++ day2/main.go
@@ -4,13 +4,78 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	filePath := "/Users/sanjeetyadav/dev/adventOfCode2023/advent-of-code-2023-go/day2/input.txt"
 	lines, err := ReadLines(filePath)
 	check(err)
-	fmt.Println(lines)
+
+	game := make(map[string]string)
+
+	for _, s := range lines {
+		gameValue := strings.Split(s, ":")
+		game[gameValue[0]] = gameValue[1]
+	}
+
+	validGames := []int{}
+
+	for k, v := range game {
+		sets := ReadSets(v)
+		isValid := true
+		for _, s := range sets {
+			fmt.Printf("calling validate for game %s and set %s\n", k, s)
+			isValid = isValid && validateSet(s)
+			fmt.Printf("%t validate for game %s and set %s\n", isValid, k, s)
+		}
+		if isValid {
+			g_num, e := strconv.Atoi(strings.Split(k, " ")[1])
+			check(e)
+			validGames = append(validGames, g_num)
+			fmt.Printf("game %s and set %d\n", k, g_num)
+		}
+	}
+
+	slices.Sort(validGames)
+
+	ans := 0
+	for _, n := range validGames {
+		ans += n
+	}
+
+	fmt.Println(ans)
+
+}
+
+func validateSet(cubes string) bool {
+	cubeValues := strings.Split(cubes, ",")
+	fmt.Println(cubeValues)
+
+	isValid := true
+	for _, c := range cubeValues {
+		countColor := strings.Split(strings.TrimSpace(c), " ")
+		num, err := strconv.Atoi(strings.TrimSpace(countColor[0]))
+		check(err)
+		fmt.Printf("validating.. %s with num : %d\n ", countColor[1], num)
+		if countColor[1] == "green" {
+			isValid = isValid && num <= 13
+		} else if countColor[1] == "blue" {
+			isValid = isValid && num <= 14
+		} else {
+			isValid = isValid && num <= 12
+		}
+		if !isValid {
+			return false
+		}
+	}
+	return isValid
+}
+
+func ReadSets(game string) []string {
+	return strings.Split(game, ";")
 }
 
 func ReadLines(filePath string) ([]string, error) {
